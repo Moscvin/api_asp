94b6599f7508f5cba532b23855b71d53e39833ed
Moscvin <moscvin.2002@gmail.com>
2024-11-24 18:25:46 +0200

    Refactor MongoDB configuration and update Ocelot routing for additional endpoints

--- Proxy/Program.cs
+++ Proxy/Program.cs
@@ -3,15 +3,12 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Adaugă fișierul de configurare `ocelot.json`
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
-// Adaugă serviciile Ocelot
 builder.Services.AddOcelot();
 
 var app = builder.Build();
 
-// Mapare middleware pentru Ocelot
 app.UseOcelot().Wait();
 
 app.Run();
